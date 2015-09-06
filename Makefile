e3d087a41b5833422008201a73153c8d1d7c3c3b
Sridhar Ratnakumar <srid@srid.ca>
2015-09-05 22:03:56 -0700

    JSON decoder working!

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ all:	server client
 	@true
 
 server:
-	go build ./src/server/
+	GOBIN=./bin go install ./src/server/
 
 client:
 	elm make src/client/SlowNews.elm
--- src/client/SlowNews.elm
+++ src/client/SlowNews.elm
@@ -1,16 +1,49 @@
 import Http
+import Html.Attributes exposing (..)
 import Signal
 import Task exposing (..)
 import Html as H
 import Html exposing (Html)
+import Json.Decode  as J
+import Json.Decode exposing ((:=))
+import Json.Encode as JE
 
-getData : Task Http.Error String
+type alias Link =
+  { title  : String
+  , url    : String
+  }
+
+type alias Site =
+  { name   : String
+  , links  : List Link
+  }
+
+type alias Model =
+  List Site
+
+andMap : J.Decoder (a -> b) -> J.Decoder a -> J.Decoder b
+andMap = J.object2 (<|)
+
+decodeLink : J.Decoder Link
+decodeLink = Link
+  `J.map`   ("title"  := J.string)
+  `andMap`  ("url"    := J.string)
+
+decodeSite : J.Decoder Site
+decodeSite = Site
+  `J.map`  ("name"    := J.string)
+  `andMap` ("links"   := J.list decodeLink)
+
+decodeModel : J.Decoder Model
+decodeModel = J.list decodeSite
+
+getData : Task Http.Error Model
 getData =
-  Http.getString "/data"
+  Http.get decodeModel "/data"
 
-dataMailbox : Signal.Mailbox String
+dataMailbox : Signal.Mailbox Model
 dataMailbox =
-  Signal.mailbox "Initializing.. "
+  Signal.mailbox [{ name = "Loading...", links = []}]
 
 port runner : Task Http.Error ()
 port runner =
@@ -20,6 +53,16 @@ main : Signal Html
 main =
   Signal.map view dataMailbox.signal
 
-view : String -> Html
-view data =
-  H.code [] [H.text data]
+view : Model -> Html
+view sites =
+  H.div [] <| List.map viewSite sites
+
+viewSite : Site -> Html
+viewSite site =
+  H.div []
+    [ H.h2 [] [H.text site.name]
+    , H.div [] <| List.map viewLink site.links ]
+
+viewLink : Link -> Html
+viewLink link =
+  H.li [] [H.a [href link.url] [H.text link.title]]
