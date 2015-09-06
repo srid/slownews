ff604a4ed294df36c35e625d8ccf602c9ecc3729
Sridhar Ratnakumar <srid@srid.ca>
2015-09-05 21:41:13 -0700

    Elm work around tasks and ports

--- src/client/SlowNews.elm
+++ src/client/SlowNews.elm
@@ -1,5 +1,25 @@
+import Http
+import Signal
+import Task exposing (..)
 import Html as H
 import Html exposing (Html)
 
-main : Html
-main = H.div [] [H.text "Hello, from Elm"]
+getData : Task Http.Error String
+getData =
+  Http.getString "/data"
+
+dataMailbox : Signal.Mailbox String
+dataMailbox =
+  Signal.mailbox "Initializing.. "
+
+port runner : Task Http.Error ()
+port runner =
+  getData `andThen` (Signal.send dataMailbox.address)
+
+main : Signal Html
+main =
+  Signal.map view dataMailbox.signal
+
+view : String -> Html
+view data =
+  H.code [] [H.text data]
