3ee7df7d6a5fe240a44989c7d6c1460600ec293f
Sridhar Ratnakumar <srid@srid.ca>
2015-10-09 17:18:28 -0700

    Heroku deployment fixes
    - Have the buildpack use Elixir 1.1
    - Read $PORT env

--- config/config.exs
+++ config/config.exs
@@ -2,7 +2,7 @@
 # and its dependencies with the aid of the Mix.Config module.
 use Mix.Config
 
-config :trot, :port, 4001
+config :trot, :port, System.get_env("PORT")
 config :trot, :router, Slownews.Router
 
 # This configuration is loaded before any dependency and is restricted
