bbed268b5d62cc0f53fa41fb7eb67edb999938d1
Sridhar Ratnakumar <srid@srid.ca>
2017-08-13 21:13:45 -0400

    Run hindent

--- frontend/Main.hs
+++ frontend/Main.hs
@@ -4,6 +4,7 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeFamilies        #-}
 {-# LANGUAGE TypeOperators       #-}
+
 module Main where
 
 import           Data.Aeson
@@ -18,96 +19,90 @@ import           Miso                          hiding (defaultOptions)
 import           Miso.String
 
 -- | Model
-data Model
-  = Model
-  { info :: Maybe APIInfo
+data Model = Model
+  { links :: Maybe Links
   } deriving (Eq, Show)
 
 -- | Action
 data Action
-  = FetchGitHub
-  | SetGitHub APIInfo
+  = FetchLinks
+  | SetLinks Links
   | NoOp
   deriving (Show, Eq)
 
--- | Main entry point
-main :: IO ()
-main = do
-  startApp App { model = Model Nothing
-               , initialAction = FetchGitHub
-               , ..
-               }
-    where
-      update = updateModel
-      events = defaultEvents
-      subs   = []
-      view   = viewModel
-
--- | Update your model
-updateModel :: Action -> Model -> Effect Action Model
-updateModel FetchGitHub m = m <# do
-  SetGitHub <$> getGitHubAPIInfo
-updateModel (SetGitHub apiInfo) m =
-  noEff m { info = Just apiInfo }
-updateModel NoOp m = noEff m
-
--- | View function, with routing
-viewModel :: Model -> View Action
-viewModel Model {..} = view
-  where
-    view = div_ [ style_ $ M.fromList [
-                  (pack "text-align", pack "center")
-                , (pack "margin", pack "200px")
-                ]
-               ] [
-        h1_ [class_ $ pack "title" ] [ text $ pack "SlowNews XHR Playground" ]
-      , button_ attrs [
-          text $ pack "Fetch JSON via XHR"
-          ]
-      , case info of
-          Nothing ->
-            div_ [] [ text $ pack "No data" ]
-          Just (APIInfo links) ->
-            div_ [] [ text $ pack $ show links ]
-      ]
-      where
-        attrs = [ onClick FetchGitHub
-                , class_ $ pack "button is-large is-outlined"
-                ] ++
-                [ disabled_ $ pack "disabled"
-                | isJust info
-                ]
-
+-- | Link
 data Link = Link
   { url   :: MisoString
   , title :: MisoString
   } deriving (Show, Eq, Generic)
 
-newtype APIInfo = APIInfo
-  [Link] deriving (Show, Eq, Generic)
+-- | Links (Full API data)
+newtype Links =
+  Links [Link]
+  deriving (Show, Eq, Generic)
 
 instance FromJSON Link where
-  parseJSON = genericParseJSON defaultOptions { fieldLabelModifier = camelTo '_' }
+  parseJSON = genericParseJSON defaultOptions {fieldLabelModifier = camelTo '_'}
 
-instance FromJSON APIInfo where
-  parseJSON = genericParseJSON defaultOptions { fieldLabelModifier = camelTo '_' }
+instance FromJSON Links where
+  parseJSON = genericParseJSON defaultOptions {fieldLabelModifier = camelTo '_'}
 
-getGitHubAPIInfo :: IO APIInfo
-getGitHubAPIInfo = do
+-- FIXME: Handle errors
+getLinks :: IO Links
+getLinks = do
   _ <- putStrLn "Fetching.."
   url <- location
   Just resp <- contents <$> xhrByteString (req url)
-  case eitherDecodeStrict resp :: Either String APIInfo of
+  case eitherDecodeStrict resp :: Either String Links of
     Left s  -> error s
     Right j -> pure j
   where
     location = do
       hostname <- getWindowLocation >>= getHostname
       return $ "http://" <> (unpack hostname) <> ":3000/data"
-    req url = Request { reqMethod = GET
-                      , reqURI = pack url
-                      , reqLogin = Nothing
-                      , reqHeaders = []
-                      , reqWithCredentials = False
-                      , reqData = NoData
-                      }
+    req url =
+      Request
+      { reqMethod = GET
+      , reqURI = pack url
+      , reqLogin = Nothing
+      , reqHeaders = []
+      , reqWithCredentials = False
+      , reqData = NoData
+      }
+
+-- | Main entry point
+main :: IO ()
+main = do
+  startApp App {model = Model Nothing, initialAction = FetchLinks, ..}
+  where
+    update = updateModel
+    events = defaultEvents
+    subs = []
+    view = viewModel
+
+-- | Update your model
+updateModel :: Action -> Model -> Effect Action Model
+updateModel FetchLinks m       = m <# do SetLinks <$> getLinks
+updateModel (SetLinks links) m = noEff m {links = Just links}
+updateModel NoOp m             = noEff m
+
+-- | View function, with routing
+viewModel :: Model -> View Action
+viewModel Model {..} = view
+  where
+    view =
+      div_
+        [ style_ $
+          M.fromList
+            [(pack "text-align", pack "center"), (pack "margin", pack "200px")]
+        ]
+        [ h1_ [class_ $ pack "title"] [text $ pack "SlowNews XHR Playground"]
+        , button_ attrs [text $ pack "Fetch JSON via XHR"]
+        , case links of
+            Nothing            -> div_ [] [text $ pack "No data"]
+            Just (Links links) -> div_ [] [text $ pack $ show links]
+        ]
+      where
+        attrs =
+          [onClick FetchLinks, class_ $ pack "button is-large is-outlined"] ++
+          [disabled_ $ pack "disabled" | isJust links]
