347117e6ce3f23cea0738c5536d5380406594655
Sridhar Ratnakumar <srid@srid.ca>
2017-12-08 16:06:31 -0500

    Serve frontend static files

--- backend/Main.hs
+++ backend/Main.hs
@@ -3,12 +3,14 @@
 
 import Text.RawString.QQ
 import Web.Scotty
+import Network.Wai.Middleware.Static
 
 main :: IO ()
 main = scotty 3000 $ do
+  middleware $ staticPolicy (noDots >-> addBase "../frontend/static")
   get "/" $ do
-    html $ mconcat ["<h1>SlowNews, ", " index page WIP</h1>"]
+    redirect "/index.html"  -- TODO: Hide index.html from address bar.
   get "/data" $ do
     text [r|
-           [{"url":"https://www.blog.google/topics/next-billion-users/building-india-first-products-and-features/","title":"Google for India: Building India-first products and features","site":"hn/india#max=1","meta_url":"https://news.ycombinator.com/item?id=15851238","created":1512475689},{"url":"https://news.ycombinator.com/item?id=15853374","title":"AMA: NY AG Schneiderman on net neutrality and protecting our voice in government","site":"hn#max=7","meta_url":"https://news.ycombinator.com/item?id=15853374","created":1512494704}]"
+           [{"url":"https://www.blog.google/topics/next-billion-users/building-india-first-products-and-features/","title":"Google for India: Building India-first products and features","site":"hn/india#max=1","meta_url":"https://news.ycombinator.com/item?id=15851238","created":1512475689},{"url":"https://news.ycombinator.com/item?id=15853374","title":"AMA: NY AG Schneiderman on net neutrality and protecting our voice in government","site":"hn#max=7","meta_url":"https://news.ycombinator.com/item?id=15853374","created":1512494704}]
            |]
