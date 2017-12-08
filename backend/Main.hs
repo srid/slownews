d41d5e39c265239a3d2887d3883240ac5c80cf51
Sridhar Ratnakumar <srid@srid.ca>
2017-12-08 15:53:21 -0500

    Server sample JSON
    as expected by frontend

--- backend/Main.hs
+++ backend/Main.hs
@@ -1,9 +1,14 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
 
+import Text.RawString.QQ
 import Web.Scotty
 
 main :: IO ()
 main = scotty 3000 $ do
-  get "/:word" $ do
-    beam <- param "word"
-    html $ mconcat ["<h1>Scotty, ", beam, " me up!</h1>"]
+  get "/" $ do
+    html $ mconcat ["<h1>SlowNews, ", " index page WIP</h1>"]
+  get "/data" $ do
+    text [r|
+           [{"url":"https://www.blog.google/topics/next-billion-users/building-india-first-products-and-features/","title":"Google for India: Building India-first products and features","site":"hn/india#max=1","meta_url":"https://news.ycombinator.com/item?id=15851238","created":1512475689},{"url":"https://news.ycombinator.com/item?id=15853374","title":"AMA: NY AG Schneiderman on net neutrality and protecting our voice in government","site":"hn#max=7","meta_url":"https://news.ycombinator.com/item?id=15853374","created":1512494704}]"
+           |]
