fe0577e9d4ea5e91e2cca8c3d6a2c4c27d513be9
Sridhar Ratnakumar <srid@srid.ca>
2017-12-08 14:12:38 -0500

    Add stack scotty template

---
+++ backend/Main.hs
@@ -0,0 +1,9 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Web.Scotty
+
+main :: IO ()
+main = scotty 3000 $ do
+  get "/:word" $ do
+    beam <- param "word"
+    html $ mconcat ["<h1>Scotty, ", beam, " me up!</h1>"]
