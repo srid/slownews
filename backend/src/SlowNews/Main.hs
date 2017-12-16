bd6ec3d90a0c8aa8de939f18727f241035846325
Sridhar Ratnakumar <srid@srid.ca>
2017-12-15 22:01:18 -0500

    Add request logging
    And use two sample subreddits

--- backend/src/SlowNews/Main.hs
+++ backend/src/SlowNews/Main.hs
@@ -1,8 +1,10 @@
 {-# LANGUAGE OverloadedStrings #-}
 
+import           Control.Concurrent (forkIO)
 import           Control.Concurrent.STM
 import           Control.Monad.IO.Class
 import           Network.Wai.Middleware.Static
+import           Network.Wai.Middleware.RequestLogger
 import           Web.Scotty
 
 import SlowNews.Reddit as Reddit
@@ -12,10 +14,17 @@ import SlowNews.Link (Link, appendLinks)
 main :: IO ()
 main = do
   links       <- atomically $ newTVar ([] :: [Link])
-  redditLinks <- Reddit.fetchSubreddit "zerocarb"
-  atomically $ appendLinks links redditLinks
 
+  _ <- forkIO $ do
+    redditLinks <- Reddit.fetchSubreddit "zerocarb"
+    atomically $ appendLinks links redditLinks
+
+  _ <- forkIO $ do
+    redditLinks <- Reddit.fetchSubreddit "programming"
+    atomically $ appendLinks links redditLinks
+  
   scotty 3000 $ do
+    middleware $ logStdoutDev
     middleware $ staticPolicy (noDots >-> addBase "../frontend/static")
     get "/" $ redirect "/index.html" -- TODO: Hide index.html from address bar.
     get "/data" $ do
