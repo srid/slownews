f8fc16ead5271902c16cfb16e7a47ae8217ac094
Sridhar Ratnakumar <srid@srid.ca>
2018-01-29 15:42:29 -0500

    Use the current production style

--- frontend/src/SlowNews/Frontend.hs
+++ frontend/src/SlowNews/Frontend.hs
@@ -1,9 +1,11 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
 
+import Data.FileEmbed
 import Language.Javascript.JSaddle.Warp
-import Reflex.Dom hiding (Link, mainWidget, run)
-import Reflex.Dom.Core (mainWidget)
+import Reflex.Dom hiding (Link, mainWidgetWithCss, run)
+import Reflex.Dom.Core (mainWidgetWithCss)
 import SlowNews.Link (Link (..))
 
 -- TODO
@@ -12,12 +14,13 @@ import SlowNews.Link (Link (..))
 -- 3. Complete UI
 
 main :: IO ()
-main = run 3001 $ mainWidget app
+main = run 3001 $ mainWidgetWithCss css app
+  where css = $(embedFile "style.css")
 
 app :: MonadWidget t m => m ()
 app = el "div" $ do
   elClass "h1" "title" $ text "SlowNews"
-  el "ul" $ do
+  el "tr" $ do
     simpleList
       (constDyn sampleLinks)
       (\d -> dyn $ displayLink <$> d)
@@ -27,10 +30,13 @@ app = el "div" $ do
 
 displayLink :: MonadWidget t m => Link -> m ()
 displayLink link_ = do
-  el "li" $ do
-    elAttr "a" ("href" =: url) $ text title
-      where url = linkUrl link_
-            title = linkTitle link_
+  el "tr" $ do
+    el "td" blank  -- Time
+    elClass "td" "meta" blank -- Comments Url
+    el "td" $ do
+      elAttr "a" ("href" =: url) $ text title
+        where url = linkUrl link_
+              title = linkTitle link_
 
 sampleLinks :: [Link]
 sampleLinks =
