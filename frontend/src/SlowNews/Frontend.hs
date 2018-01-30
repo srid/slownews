154edb0ec44f1e174d0089866345e943a562c14e
Sridhar Ratnakumar <srid@srid.ca>
2018-01-30 13:04:49 -0500

    Complete UI

--- frontend/src/SlowNews/Frontend.hs
+++ frontend/src/SlowNews/Frontend.hs
@@ -2,18 +2,18 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TemplateHaskell #-}
--- 1. DONE Write basic UI
--- 2. DONE Shell + normal mode
--- 2. DONE XHR
--- 3. TODO Complete UI
 
 import Control.Lens
 import Control.Monad
 import Data.Default
 import Data.FileEmbed
+import Data.Function (on)
+import Data.List (sortBy)
 import Data.Maybe
 import Data.Monoid
 import Data.Text as T
+import Data.Time.Clock.POSIX (posixSecondsToUTCTime)
+import Data.Time.Format (defaultTimeLocale, formatTime)
 import Reflex.Dom hiding (Link, mainWidgetWithCss)
 
 import SlowNews.Link (Link (..))
@@ -28,7 +28,7 @@ app = el "div" $ do
   elClass "h1" "title" $ text appTitle
   linksDyn <- getLinks
   el "tr" $ do
-    simpleList linksDyn displayLink
+    simpleList (sortLinks <$> linksDyn) displayLink
   divClass "footer" $ do
     elAttr "a" ("href" =: "https://github.com/srid/slownews") $ do
       text "SlowNews source on GitHub"
@@ -43,11 +43,16 @@ getLinks = do
   holdDyn [loadingLink] $ fmapMaybe (id <$>) resp
     where loadingLink = Link "Loading..." "" "" 0 ""
 
+sortLinks :: [Link] -> [Link]
+sortLinks = sortBy (flip compare `on` linkCreated)
+
 displayLink :: MonadWidget t m => Dynamic t Link -> m ()
 displayLink dLink = el "tr" $ do
-  el "td" blank  -- Time
-  elClass "td" "meta" blank -- Comments Url
-  el "td" $ do
-    elDynAttr "a" dAttr $ dynText dTitle
-      where dTitle = linkTitle <$> dLink
-            dAttr = ffor dLink $ \l -> "href" =: linkUrl l
+  el "td" $ dynText (dayOfWeek . linkCreated <$> dLink)
+  elClass "td" "meta" $ dynA (linkMetaUrl <$> dLink) (linkSite <$> dLink)
+  el "td" $ dynA (linkUrl <$> dLink) (linkTitle <$> dLink)
+    where dayOfWeek = T.pack . formatTime defaultTimeLocale "%a" . posixSecondsToUTCTime . fromIntegral
+
+dynA :: MonadWidget t m => Dynamic t T.Text -> Dynamic t T.Text -> m ()
+dynA url title = elDynAttr "a" dAttr $ dynText title
+  where dAttr = ffor url $ \u -> "href" =: u
