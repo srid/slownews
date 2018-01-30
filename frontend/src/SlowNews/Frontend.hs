78b1c133eb80da051f7bda8b561d600ea671a800
Sridhar Ratnakumar <srid@srid.ca>
2018-01-30 12:29:41 -0500

    Complete XHR work

--- frontend/src/SlowNews/Frontend.hs
+++ frontend/src/SlowNews/Frontend.hs
@@ -1,19 +1,19 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TemplateHaskell #-}
 -- 1. DONE Write basic UI
 -- 2. DONE Shell + normal mode
--- 2. TODO XHR
+-- 2. DONE XHR
 -- 3. TODO Complete UI
 
-import Data.FileEmbed
-import Data.Maybe
-import Data.Text as T
 import Control.Lens
 import Control.Monad
 import Data.Default
+import Data.FileEmbed
+import Data.Maybe
 import Data.Monoid
+import Data.Text as T
 import Reflex.Dom hiding (Link, mainWidgetWithCss)
 
 import SlowNews.Link (Link (..))
@@ -26,23 +26,24 @@ main = run2 $ mainWidgetWithCss css app
 app :: MonadWidget t m => m ()
 app = el "div" $ do
   elClass "h1" "title" $ text appTitle
-  getLinks
+  linksDyn <- getLinks
   el "tr" $ do
     simpleList
-      (constDyn sampleLinks)
+      linksDyn
       (\d -> dyn $ displayLink <$> d)
   divClass "footer" $ do
     elAttr "a" ("href" =: "https://github.com/srid/slownews") $ do
       text "SlowNews source on GitHub"
 
 -- | Fetch links from the server
-getLinks :: MonadWidget t m => m ()
+getLinks :: MonadWidget t m => m (Dynamic t [Link])
 getLinks = do
   pb <- getPostBuild
-  let req = xhrRequest "GET" "/data" def
-  resp :: Event t (Maybe T.Text) <- fmap (fmap _xhrResponse_responseText) $ performRequestAsync $ req <$ pb
-  text "XHR Response => "
-  dynText <=< holdDyn "" $ fmapMaybe (id <$>) resp
+  let urlEvent = "/data" <$ pb
+  -- TODO: error handling
+  resp :: Event t (Maybe [Link]) <- getAndDecode urlEvent
+  holdDyn [loadingLink] $ fmapMaybe (id <$>) resp
+    where loadingLink = Link "Loading..." "" "" 0 ""
 
 displayLink :: MonadWidget t m => Link -> m ()
 displayLink link_ = do
