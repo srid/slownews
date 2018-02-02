2bb934dada3bc5b59884adca5cddca6c4cc55065
Sridhar Ratnakumar <srid@srid.ca>
2018-02-02 08:33:49 -0500

    Nix: add ios and android stanzas
    Not yet tested, though android build works.

--- default.nix
+++ default.nix
@@ -10,4 +10,16 @@
     ghc = ["common" "backend" "frontend"];
     ghcjs = ["common" "frontend"];
   };
+
+  android.frontend = {
+    executableName = "slownews-frontend";
+    applicationId = "ca.srid.slownews";
+    displayName = "SlowNews";
+  };
+
+  ios.frontend = {
+    executableName = "slownews-frontend";
+    bundleIdentifier = "ca.srid.slownews";
+    bundleName = "SlowNews";
+  };
 })
