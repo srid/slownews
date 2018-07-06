11ea464d691268f6099ce8777338b3587720d61b
Sridhar Ratnakumar <srid@srid.ca>
2018-07-05 23:05:01 -0400

    Finish the Clay port

--- frontend/src/Frontend.hs
+++ frontend/src/Frontend.hs
@@ -3,18 +3,21 @@
 {-# LANGUAGE TypeApplications #-}
 module Frontend where
 
+import Data.Semigroup ((<>))
 import qualified Data.Text as T
 
 import Reflex.Dom.Core
 
+import Static
+
 import Frontend.App (app)
-import Frontend.CSS (cssInline, appCssStr)
+import Frontend.CSS (appCssStr)
 
 frontend :: (StaticWidget x (), Widget x ())
 frontend = (head', body)
   where
     head' = do
       el "title" $ text "SlowNews"
-      el "style" $ text cssInline
+      elAttr "link" ("rel" =: "stylesheet" <> "type" =: "text/css" <> "href" =: static @"semantic.min.css") blank
       el "style" $ text appCssStr
     body = app
--- frontend/src/Frontend/CSS.hs
+++ frontend/src/Frontend/CSS.hs
@@ -1,9 +1,9 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module Frontend.CSS (cssInline, appCssStr) where
+module Frontend.CSS (appCssStr) where
 
-import Data.Text (Text)
 import Data.String.Conv (toS)
+import Data.Text (Text)
 
 import Clay
 
@@ -15,6 +15,9 @@ themeColor = lightgreen
 
 appCss :: Css
 appCss = do
+  importUrl "https://fonts.googleapis.com/css?family=Muli"
+  importUrl "https://fonts.googleapis.com/css?family=Autour+One"
+
   html ? do
     sym2 margin (px 0) auto
     fontFamily ["Muli"] [sansSerif]
@@ -25,49 +28,34 @@ appCss = do
     backgroundColor themeColor
     sym padding $ em 0.3
 
--- TODO: Include semantic.min.css in repo
+  td ? do
+    sym padding $ em 0.2
+    paddingBottom $ em 0.5
 
--- TODO: Switch to clay
-cssInline :: Text
-cssInline = "\
-\ @import url(https://fonts.googleapis.com/css?family=Muli); \
-\ @import url(https://fonts.googleapis.com/css?family=Autour+One); \
-\ @import url(https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.1/semantic.min.css); \
-\  \
-\ td { \
-\     padding: 0.2em; \
-\     padding-bottom: 0.5em; \
-\ } \
-\  \
-\ td.meta { \
-\     text-align: right; \
-\     padding-right: 0.5em; \
-\ } \
-\  \
-\ a { \
-\     text-decoration: none; \
-\ } \
-\ a:visited { \
-\     text-decoration: line-through; \
-\     color: gray; \
-\ } \
-\ a:hover { \
-\     background-color: #efefef; \
-\ } \
-\  \
-\ .meta a { \
-\     font-size: 85%; \
-\     text-decoration: none; \
-\     color: lightgreen; \
-\     background-color: #444; \
-\     padding: 0.2em 1em; \
-\ } \
-\  \
-\ .footer { \
-\     text-align: center; \
-\     padding-top: 1em; \
-\     margin-top: 2em; \
-\     margin-bottom: 2em; \
-\     border-top: solid gray; \
-\ } \
-\ "
+  "td.meta" ? do
+    textAlign $ alignSide sideRight
+    color gray
+
+  a ? do
+    textDecoration none
+
+  "a:visited" ? do
+    textDecoration lineThrough
+    color gray
+
+  "a:hover" ? do
+    backgroundColor "#efefef"
+
+  ".meta a" ? do
+    fontSize $ pct 85
+    textDecoration none
+    color lightgreen
+    backgroundColor "#444"
+    sym2 padding (em 0.2) (em 1)
+
+  ".footer" ? do
+    textAlign center
+    paddingTop $ em 1
+    marginTop $ em 2
+    marginBottom $ em 2
+    borderTop solid (px 1) gray
