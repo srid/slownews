712fbb3ea1e36956ceee21c1d0da1bbc60785bb0
Sridhar Ratnakumar <srid@srid.ca>
2018-09-13 15:36:04 -0400

    Fix ghcjs build

--- default.nix
+++ default.nix
@@ -18,6 +18,17 @@ project ./. ({ pkgs, ... }: {
   };
 
   overrides = self: super: with pkgs.haskell.lib; {
+    Glob = dontCheck super.Glob;
+    aeson-casing = dontCheck super.aeson-casing;
     clay = dontCheck super.clay;
+    cryptohash-md5 = dontCheck super.cryptohash-md5;
+    cryptohash-sha1 = dontCheck super.cryptohash-sha1;
+    http2 = dontCheck super.http2;
+    iproute = dontCheck super.iproute;
+    mockery = dontCheck super.mockery;
+    simple-sendfile = doJailbreak super.simple-sendfile;
+    wai-app-static = dontCheck super.wai-app-static;
+    wai-extra = dontCheck super.wai-extra;
+    word8 = dontCheck super.word8;
   };
 })
