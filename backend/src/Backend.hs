905c401eefb66854283a73e6ae615f7759b0c5e0
Sridhar Ratnakumar <srid@srid.ca>
2018-09-13 09:54:03 -0400

    Case on backend routes

--- backend/src/Backend.hs
+++ backend/src/Backend.hs
@@ -1,27 +1,20 @@
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeFamilies #-}
 module Backend where
 
 import Control.Lens
 import Control.Monad.IO.Class (liftIO)
 import Data.Aeson
-import qualified Data.ByteString.Char8 as BSC8
 import qualified Data.ByteString.Lazy as BSL
-import Data.Default (Default (..))
-import Data.Text (Text)
-import System.IO (BufferMode (..), hSetBuffering, stderr)
+import Data.Dependent.Sum (DSum (..))
 
-import Reflex.Dom (renderStatic)
-
-import Snap (Snap, commandLineConfig, defaultConfig, httpServe, route, writeBS)
-import Snap.Internal.Http.Server.Config (Config (accessLog, errorLog), ConfigLog (ConfigIoLog))
+import Snap (writeBS)
 
 import Obelisk.Backend
-import Obelisk.Route
-import Obelisk.Snap
 
 import Common.Route
-import Frontend
 
 import Backend.Entrypoint (start)
 
@@ -29,8 +22,9 @@ backend :: Backend BackendRoute Route
 backend = Backend
   { _backend_run = \serve -> do
       getLinks <- liftIO start
-      serve $ \_ -> do  -- "/data"
-        links <- liftIO getLinks
-        writeBS $ BSL.toStrict $ encode links
+      serve $ \case
+        BackendRoute_GetData :=> Identity () -> do
+          links <- liftIO getLinks
+          writeBS $ BSL.toStrict $ encode links
   , _backend_routeEncoder = backendRouteEncoder
   }
